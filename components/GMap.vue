e924267264e1b4210dd0af8893761ddcfce2508d
aner87 <andrea.concari@icloud.com>
2021-11-19 15:48:45 +0000

    Update components/GMap.vue, package.json, components/GMapMarker.vue, lib/plugin.js, lib/module.js

--- components/GMap.vue
+++ components/GMap.vue
@@ -1,139 +1,148 @@
 <template>
   <div class="GMap">
     <div ref="map" class="GMap__Wrapper"></div>
-    <slot/>
+    <slot />
   </div>
 </template>
 
 <script>
-import GoogleMapsApiLoader from 'google-maps-api-loader'
-const MarkerClusterer = require('node-js-marker-clusterer');
+import { Loader } from "@googlemaps/js-api-loader";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
 export default {
   props: {
     options: {
       type: Object,
-      default: () =>{
-        return {}
-      }
+      default: () => {
+        return {};
+      },
     },
     language: {},
     zoom: {
       type: Number,
-      default: 10
+      default: 10,
     },
     cluster: {
       type: Object,
-      default: () =>{
-        return {}
-      }
+      default: () => {
+        return {};
+      },
     },
     center: {
       type: Object,
       default: () => {
         return {
-          lat: 45.8150,
-          lng: 15.9819
-        }
-      }
-    }
+          lat: 45.815,
+          lng: 15.9819,
+        };
+      },
+    },
   },
 
-  data(){
-    return{
+  data() {
+    return {
       map: null,
       google: null,
       markerCluster: null,
       markers: [],
       events: [
-        'bounds_changed',
-        'center_changed',
-        'click',
-        'dblclick',
-        'drag',
-        'dragend',
-        'dragstart',
-        'heading_changed',
-        'idle',
-        'maptypeid_changed',
-        'mousemove',
-        'mouseout',
-        'mouseover',
-        'projection_changed',
-        'resize',
-        'rightclick',
-        'tilesloaded',
-        'tilt_changed',
-        'zoom_changed',
-      ]
-    }
+        "bounds_changed",
+        "center_changed",
+        "click",
+        "dblclick",
+        "drag",
+        "dragend",
+        "dragstart",
+        "heading_changed",
+        "idle",
+        "maptypeid_changed",
+        "mousemove",
+        "mouseout",
+        "mouseover",
+        "projection_changed",
+        "resize",
+        "rightclick",
+        "tilesloaded",
+        "tilt_changed",
+        "zoom_changed",
+      ],
+    };
   },
 
-	async mounted(){
-    if(this.$GMaps.loaded === false){
+  async mounted() {
+    if (this.$GMaps.loaded === false) {
       this.$GMaps.loaded = true;
-      try{
+      try {
         let GMapSettings = {
           apiKey: this.$GMaps.apiKey,
-          language: this.language
+          language: this.language,
+        };
+
+        if (this.$GMaps.libraries !== undefined) {
+          GMapSettings["libraries"] = this.$GMaps.libraries;
         }
 
-        if(this.$GMaps.libraries !== undefined){
-          GMapSettings['libraries'] = this.$GMaps.libraries
-        }
-
-        const google = GoogleMapsApiLoader(GMapSettings)
+        const loader = new Loader(GMapSettings);
+        const google = await loader.load();
         this.$GMaps.google = google;
-      } catch(e){
-      }
+      } catch (e) {}
     }
 
     this.google = await this.$GMaps.google;
     this.initMap();
-    this.$emit('init', this.google);
-    this.$emit('loaded', this.google);
+    this.$emit("init", this.google);
+    this.$emit("loaded", this.google);
   },
 
-  beforeDestroy(){
+  beforeDestroy() {
     this.$GMaps.loaded = false;
   },
 
   methods: {
-    initMap(){
+    initMap() {
       this.map = new google.maps.Map(this.$refs.map, {
         center: this.center,
         zoom: this.zoom,
-        ...this.options
+        ...this.options,
       });
 
       this.initChildren();
-      this.events.forEach(event =>{
-        this.map.addListener(event, (e) =>{
-          this.$emit(event, {map: this.map, event: e})
+      this.events.forEach((event) => {
+        this.map.addListener(event, (e) => {
+          this.$emit(event, { map: this.map, event: e });
         });
-      })
+      });
     },
 
-    initChildren(){
-      if(this.markerCluster !== null) this.markerCluster.clearMarkers()
-      if(this.markers.length > 0) this.markers = [];
+    initChildren() {
+      if (this.markerCluster !== null) this.markerCluster.clearMarkers();
+      if (this.markers.length > 0) this.markers = [];
 
-      this.$children.forEach(child =>{
+      this.$children.forEach((child) => {
         child.init();
       });
 
-      this.map['markers'] = this.markers;
+      this.map["markers"] = this.markers;
 
-      if(Object.keys(this.cluster).length > 0){
-        this.markerCluster = new MarkerClusterer(this.map, this.markers, {...this.cluster.options});
+      if (Object.keys(this.cluster).length > 0) {
+        const map = this.map;
+        const markers = this.markers;
+        const clusterOptions = this.cluster.options;
+
+        this.markerCluster = new MarkerClusterer({
+          map,
+          markers,
+          clusterOptions,
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style>
-	.GMap__Wrapper{
-    width: 100%;
-    height: 400px;
-  }
+.GMap__Wrapper {
+  width: 100%;
+  height: 400px;
+}
 </style>
